4feabfd63fa7e2ef61eec417642cd5d74c5797bc
Roshankumar0808 <91427277+Roshankumar0808@users.noreply.github.com>
2021-09-29 14:01:22 +0530

    docs:many updates

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import "./styles.css";
 
 import App from "./App";
 
 const rootElement = document.getElementById("root");
+const Footer = () => (
+  <footer className="footer">
+    <p>Made by Roshan,follow me on:</p>
+    <a class="link" href="https://www.instagram.com/roshan4445/">
+      Instagram
+    </a>
+  </footer>
+);
+// ReactDOM.render(
+//   [<App key="1" />, <Footer key="2" />],
+//   document.getElementById("root")
+// );
 ReactDOM.render(
   <StrictMode>
-    <App />
+    <App key="1" />, <Footer key="2" />
+    {/* document.getElementById("root") */}
   </StrictMode>,
   rootElement
 );
